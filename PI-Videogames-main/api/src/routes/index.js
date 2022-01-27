829192d5ad267c55d51f8cd343135c63a1907424
diemora473 <diego.e.morales01@gmail.com>
2022-01-27 02:18:03 -0300

    IniciandoBack

--- PI-Videogames-main/api/src/routes/index.js
+++ PI-Videogames-main/api/src/routes/index.js
@@ -1,12 +1,25 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const axios = require('axios')
+const videogamesRouter = require('./videogames')
+const genresRouter = require('./genres')
+const idRouter = require('./vgamesByid')
+const platform = require('./platforms')
+const create = require('./createVgames')
 
 const router = Router();
 
+router.use('/videogames', videogamesRouter)
+router.use('/genres', genresRouter)
+router.use('/videogame', idRouter)
+router.use('/platform', platform)
+router.use('/videogames', create)
+
 // Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
+// Ejemplo: router.use('/auth', authRouter);//En este caso no está modularizado!!!
+
+
 
 
 module.exports = router;
