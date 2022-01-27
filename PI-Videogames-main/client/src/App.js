b197f4834fdd27fee4dc43af9d70a3cf27b1cb5b
diemora473 <diego.e.morales01@gmail.com>
2022-01-27 03:00:14 -0300

    YaEmpezamosFront

--- PI-Videogames-main/client/src/App.js
+++ PI-Videogames-main/client/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import VgameCreate from './componentes/VgameCreate'
+import LandingPage from './component/Landing/LandindPage'
+// import Home from './componentes/Home';
+// import Detail from './componentes/Detail'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Routes>
+
+          <Route exact path='/' element={<LandingPage />} />
+          {/* <Route exact path='/home' component={Home} /> */}
+          {/* <Route path='/creategame' component={VgameCreate}/> */}
+          {/* <Route path='/videogame/:id' component={Detail}/> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
